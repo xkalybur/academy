5d74a5465e8bbdd577c7577c963301a50e10b954
roxas2019 <roxas.kh.2019@gmail.com>
2019-09-19 15:46:01 -0500

    Creacion de Website

--- __manifest__.py
+++ __manifest__.py
@@ -20,13 +20,14 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base'],
+    'depends': ['base','website','website_sale'],
 
     # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
-        'views/views.xml',
+        'security/ir.model.access.csv',
         'views/templates.xml',
+        'views/views.xml',
+        'views/data.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
--- models/models.py
+++ models/models.py
@@ -2,14 +2,15 @@
 
 from odoo import models, fields, api
 
-# class academy(models.Model):
-#     _name = 'academy.academy'
+class Teachers(models.Model):
+    _name = 'academy.teachers'
 
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         self.value2 = float(self.value) / 100
+    name = fields.Char()
+    biography = fields.Html()
+    course_ids = fields.One2many('product.template', 'teacher_id', string="Courses")
+
+class Courses(models.Model):
+    _inherit = 'product.template'
+
+    name = fields.Char()
+    teacher_id = fields.Many2one('academy.teachers', string="Teacher")
